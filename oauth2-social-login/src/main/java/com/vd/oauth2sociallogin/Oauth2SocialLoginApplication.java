efae662ef9558d288d062dd523d1321bba980274
Deshmukh <vivek.b.deshmukh@capgemini.com>
2019-12-10 10:59:46 +0530

    Initial Commit

---
+++ oauth2-social-login/src/main/java/com/vd/oauth2sociallogin/Oauth2SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.vd.oauth2sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SocialLoginApplication.class, args);
+	}
+
+}
---
+++ oauth2-social-login/src/test/java/com/vd/oauth2sociallogin/Oauth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.oauth2sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
